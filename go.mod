12f407f7cc70f5c006d216ec2af4095fa7232a4a
lihaifeng <haifengli@miliantech.com>
2019-10-17 19:03:29 +0800

    initial commit

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module PennyHardway
 go 1.12
 
 require (
+	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b // indirect
 	github.com/gin-gonic/gin v1.4.0
+	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/jinzhu/gorm v1.9.11
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/silenceper/wechat v2.0.0+incompatible
 )
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
-import "C"
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"net/http"
-	"strconv"
+	"github.com/silenceper/wechat"
+	"github.com/silenceper/wechat/message"
+	"log"
 )
 
 var db *gorm.DB
@@ -19,110 +19,43 @@ func init() {
 		panic("failed to connect database")
 	}
 	//Migrate the schema
-	db.AutoMigrate(&todoModel{})
+	//db.AutoMigrate(&todoModel{})
 }
 
-type (
-	// 定义原始的数据库字段
-	todoModel struct {
-		gorm.Model
-		Title		string `json:"title"`
-		Completed   int		`json:"completed"`
-	}
-	// 处理返回的字段
-	transformedTodo struct {
-		ID          uint	`json:"id"`
-		Title		string	`json:"title"`
-		Completed   bool    `json:"completed"`
-	}
-)
+func hello(c *gin.Context) {
 
-func createTodo(c *gin.Context) {
-	completed, _ := strconv.Atoi(c.PostForm("completed"))
-	todo := todoModel{Title: c.PostForm("title"), Completed: completed}
-	db.Save(&todo)
-	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated,
-		"message": "Todo item created successfully!", "resourceId": todo.ID})
-}
+	// 配置微信参数
+	config := &wechat.Config {
+		AppID:			"wxb49b036278e9e065",
+		AppSecret:		"e91664672a12386530600cb5bb30ed84",
+		Token: 			"skipmapledrewlee",
+		EncodingAESKey:	"YCPnsvCg5oihIRrTALYDugXwwPekWcc3cjQXcUXefds",
+	}
+	wc := wechat.NewWechat(config)
 
-// fetchAllTodo 获取所有Todo
-func fetchAllTodo(c *gin.Context) {
-	var todos []todoModel
-	var _todos []transformedTodo
-	db.Find(&todos)
-	if len(todos) <= 0 {
-		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+	// 传入request和responseWriter
+	server := wc.GetServer(c.Request, c.Writer)
+	// 设置接收消息的处理方法
+	server.SetMessageHandler(func(msg message.MixMessage) *message.Reply {
+		// 回复消息: 演示回复用户发送的消息
+		text := message.NewText(msg.Content)
+		return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
+	})
+
+	// 处理消息接收及回复
+	err := server.Serve()
+	if err != nil{
+		log.Println("handle message receive err: ", err)
 		return
 	}
-	//对todos的属性做一些转换以构建更好的响应体
-	for _, item := range todos {
-		completed := false
-		if item.Completed == 1 {
-			completed = true
-		} else {
-			completed = false
-		}
-		_todos = append(_todos, transformedTodo{ID: item.ID, Title: item.Title, Completed: completed})
-	}
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todos})
-}
 
-// fetchSingleTodo 获取单个Todo
-func fetchSingleTodo(c *gin.Context) {
-	var todo todoModel
-	todoID := c.Param("id")
-	db.First(&todo, todoID)
-	if todo.ID == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
-		return
-	}
-	completed := false
-	if todo.Completed == 1 {
-		completed = true
-	} else {
-		completed = false
-	}
-	_todo := transformedTodo{ID: todo.ID, Title: todo.Title, Completed: completed}
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todo})
-}
-
-// updateTodo 更新单个todo
-func updateTodo(c *gin.Context) {
-	var todo todoModel
-	todoID := c.Param("id")
-	db.First(&todo, todoID)
-	if todo.ID == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
-		return
-	}
-	db.Model(&todo).Update("title", c.PostForm("title"))
-	completed, _ := strconv.Atoi(c.PostForm("completed"))
-	db.Model(&todo).Update("completed", completed)
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo updated successfully!"})
-}
-
-// deleteTodo 删除一个todo
-func deleteTodo(c *gin.Context) {
-	var todo todoModel
-	todoID := c.Param("id")
-	db.First(&todo, todoID)
-	if todo.ID == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
-		return
-	}
-	db.Delete(&todo)
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo deleted successfully!"})
+	// 发送恢复的消息
+	server.Send()
 }
 
 func main() {
 	router := gin.Default()
-	v1 := router.Group("/api/v1/")
-	{
-		v1.POST("/", createTodo)
-		v1.GET("/", fetchAllTodo)
-		v1.GET("/:id", fetchSingleTodo)
-		v1.PUT("/:id", updateTodo)
-		v1.DELETE("/:id", deleteTodo)
-	}
-	router.Run()
+
+	router.Any("/", hello)
+	router.Run(":12080")
 }
